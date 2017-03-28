c47f80124d4a6c1fdfdd4b3fdd96b864b05d037f
Anastasiia Zolochevska <anastasiia.zolochevska@gmail.com>
2017-03-27 18:10:32 -0700

    Appinsights

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var azureStorageService = require('./azureStorageService.js');
+var appInsights = require("applicationinsights");
+
+appInsights.setup().setAutoCollectExceptions(true).start();
+var appInsightsClient = appInsights.getClient();
 
 var port = process.env.PORT || 3000;
 
-var app = express()
+var app = express();
 
 app.use('/dashboard', express.static('public'));
+
 app.get('/rtt', function (req, res) {
     var location = req.param("location");
     var count = req.param.count || 100;
-    azureStorageService.getLatestEntries(count, location).then(result => res.send(result));
+    azureStorageService.getLatestEntries(count, location).then(result => res.send(result), error=>appInsightsClient.trackTrace(error));
 });
 
 app.listen(port, function () {
